7cdb5c7cab1ef608bccf17b653882481ec1fd0b5
brunomori <brunomanzinimori@Gmail.com>
2022-10-17 12:36:27 -0300

    att

---
+++ script.js
@@ -0,0 +1,28 @@
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('img')
+    var boa =window.document.getElementById('boa')
+    var data = new Date()
+    var dia = data.getDate()
+    var mes = (data.getMonth()+1) 
+    var hora = data.getHours()
+    var min= data.getMinutes()
+    var ano= data.getFullYear()
+
+    msg.innerHTML = ` Hoje é dia ${dia} do ${mes} de ${ano} e agora são: ${hora}:${min}`
+
+    if (hora >=0 && hora <12){
+        img.src='img/manhaa.jpg'
+        boa.innerHTML =`Tenha um otimo dia!`
+        document.body.style.background='#A4D8FC'
+    } else if (hora >=12 && hora <=18 ) {
+        img.src='img/tardee.jpg'
+        boa.innerHTML ='Tenha uma otima tarde!'
+        document.body.style.background='#FFB784'
+    }
+    else{
+        img.src='img/noitee.jpg'
+        document.body.style.background = '#8568A1'
+        boa.innerHTML ='Tenha uma otima Noite!'
+    }
+}
